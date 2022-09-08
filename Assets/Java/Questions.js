378de4cd5205a1b0be3b7434922b1f0c86b1e623
Jon Beck <jonnydbeck@gmail.com>
2022-09-08 01:56:55 -0400

    Question Functionality Added

---
+++ Assets/Java/Questions.js
@@ -0,0 +1,23 @@
+//Originally I was going to have a file where I kept all the questions as text that Script.js would get
+//That didnt work however
+//I still wanted to keep them seperate and expandable so here they are
+
+var allQuestions = [];
+
+allQuestions[0] = {
+    question: "What is HTML an acronym for",
+    ans: "Hyper Text Markup Language",
+    wrong: ["Heavy Turing Markup Language", "Higher Test Machine Language", "Nothing, it just the initals of the 2 guys who created it"]
+}
+
+allQuestions[1] = {
+    question: "Which of these languages has variables",
+    ans: "CSS and Javascript",
+    wrong: ["HTML", "CSS", "JavaScript", "HTML and JavaScript"]
+}
+
+allQuestions[2] = {
+    question: "The Section and Article HTML tags do the same thing",
+    ans: "True",
+    wrong: ["False"]
+}
--- Assets/Java/Script.js
+++ Assets/Java/Script.js
@@ -14,15 +14,24 @@ var scorePg = document.querySelector("#Score");
 var timeFull = document.querySelector(".highRight");
 var timer = document.querySelector("#time");
 
+//Question elements
+var question = document.querySelector("#hQuest");
+var questionAppend = document.querySelector("#btnAppend");
 
+//Other Vars
 var points = 0;
 //Current page is based on var
 //0 = Start, 1 = Quiz, 2 = End, 3 = High
 var page = 0;
 var totalQ = 0;
+var mSecondsLeft = 0;
+var usedQuestions = [];
+
 pageStart();
 
+
 //Page Functions
+//Sets which "Page" we are on
 function pageStart(){
     page = 0;
 
@@ -56,12 +65,14 @@ function pageHigh(){
     scorePg.style.display = "block";
 }
 
+//hides all pages
 function pageReset(){
     beginPg.style.display = "none";
     questionPg.style.display = "none";
     endPg.style.display = "none";
     scorePg.style.display = "none";
 }
+
 
 //Button onClick Functions
 highBtn.addEventListener("click", function(event) {
@@ -81,8 +92,9 @@ highBtn.addEventListener("click", function(event) {
 
 });
 
-//TODO Quiz Functionality
 startBtn.addEventListener("click", function(event) {
+    totalQ = 9;
+    usedQuestions = [];
     timeStart();
     giveQuestion();
 });
@@ -96,7 +108,8 @@ subBtn.addEventListener("click", function(event) {
 
 //Other Functions
 function timeStart(){
-    var mSecondsLeft = 6000;
+    mSecondsLeft = 6000;
+
 
     var timerInterval = setInterval(function() {
         mSecondsLeft--;
@@ -117,13 +130,82 @@ function timeStart(){
 }
 
 function giveQuestion(){
-
+    generateQuestionPage();
     pageQuiz();
 
     //found this online, works like a charm
     document.querySelectorAll('#ansBtn').forEach(item => {
-        item.addEventListener('click', event => {
-            pageEnd(points);
+        item.addEventListener('click', clickevent => {
+            
+
+            if (totalQ == 0){
+                points += Math.round(mSecondsLeft/2);
+                pageEnd();
+            }
+            else{
+                totalQ--;
+                giveQuestion();
+            }
         })
     })
 }
+
+//Question Generation Functions
+function generateQuestionPage(){ 
+    //removing all previous answers
+    var prevQuestions = document.querySelectorAll('[id=ansBtn]')
+    for(i = 0; i < prevQuestions.length; i++){
+        prevQuestions[i].remove();
+    }
+
+    //Getting a number of a question
+    //If question already asked, different number is generated
+    do{
+        var currQuestion = Math.floor(Math.random() * getQuestionAmount());
+        break;
+    } while (usedQuestions.includes(currQuestion));
+    
+    //Giving the question
+    usedQuestions.push(currQuestion);
+    question.textContent = ((10 - totalQ) + ". " + getObjectQuestion(currQuestion));
+
+    //Making and shuffleing an array of answers
+    var totalAnswers = [];
+    totalAnswers = totalAnswers.concat(allQuestions[currQuestion].wrong);
+    totalAnswers.push(getObjectAnswer(currQuestion));
+    shuffle(totalAnswers);
+
+    //Adding a button element for each answer
+    for(i = 0; i < totalAnswers.length; i++){
+        var ansEL = document.createElement("Button");
+        ansEL.textContent = totalAnswers[i];
+        ansEL.setAttribute('id','ansBtn');
+        questionAppend.appendChild(ansEL);
+    }
+}
+
+//Fisher–Yates shuffle
+function shuffle(array) {
+    let currentIndex = array.length,  randomIndex;
+  
+    // While there remain elements to shuffle.
+    while (currentIndex != 0) {
+  
+      // Pick a remaining element.
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex--;
+  
+      // And swap it with the current element.
+      [array[currentIndex], array[randomIndex]] = [
+        array[randomIndex], array[currentIndex]];
+    }
+  
+    return array;
+  }
+
+
+//These are just simpler for me to program with
+//I however got a wird error by parsing the erong answers through a function
+function getObjectQuestion(num){ return allQuestions[num].question }
+function getObjectAnswer(num){ return allQuestions[num].ans}
+function getQuestionAmount(){return allQuestions.length}
