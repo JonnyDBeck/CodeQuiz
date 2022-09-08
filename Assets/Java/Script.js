4f15cd14fb1e83f325b38475807a528d91468e73
Jon Beck <jonnydbeck@gmail.com>
2022-09-08 13:24:02 -0400

    Input Box Functionality Added

--- Assets/Java/Script.js
+++ Assets/Java/Script.js
@@ -20,6 +20,7 @@ var questionAppend = document.querySelector("#btnAppend");
 
 //End Elements
 var endPoints = document.querySelector("#endPoints");
+var inName = document.querySelector("#inName");
 
 //Other Vars
 var points = 0;
@@ -31,48 +32,48 @@ var mSecondsLeft = 0;
 var usedQuestions = [];
 var currQuestion = 0;
 
+//starts user off on start page
 pageStart();
 
 
 //Page Functions
 //Sets which "Page" we are on
 function pageStart(){
+    //changes page to start page
     page = 0;
-
     timeFull.style.color = "#2D3047";
     pageReset();
     beginPg.style.display = "block";
 }
 
 function pageQuiz(){
+    //changes page to Quiz page
     page = 1;
-
     timeFull.style.color = "#4DA1A9";
     pageReset();
     questionPg.style.display = "block";
 }
 
 function pageEnd(points){
+    //changes page to score page, uises points for page content
     page = 2;
-
     endPoints.textContent = points;
-
     timeFull.style.color = "#2D3047";
     pageReset();
     endPg.style.display = "block";
 }
 
 function pageHigh(){
+    //changes page to highscore page
     page = 3;
-
     timeFull.style.color = "#2D3047";
     highBtn.textContent = "Start Page"
     pageReset();
     scorePg.style.display = "block";
 }
 
-//hides all pages
 function pageReset(){
+    //hides all pages
     beginPg.style.display = "none";
     questionPg.style.display = "none";
     endPg.style.display = "none";
@@ -84,50 +85,71 @@ function pageReset(){
 highBtn.addEventListener("click", function(event) {
 
     if(page == 3){
+        //changing from high score page to start page
         highBtn.textContent = "HighScores"
         pageStart();
     }
     else if (page == 2 || page == 1){
+        //changing from either quiz or end page
+        //asks if they are sure
         if(window.confirm("Are you sure?\nYour Progress will be lost!")){
             pageHigh();
         }
     }
     else{
+        //changing from start to highscore page
         pageHigh();
     }
 
 });
 
 startBtn.addEventListener("click", function(event) {
+    //Starts test by reseting variables
     points = 0;
     totalQ = 9;
     usedQuestions = [];
+
+    //starts test by calling functions
     timeStart();
     giveQuestion();
 });
 
 subBtn.addEventListener("click", function(event) {
-
-
-    pageHigh();
+    //submits score with name
+    
+    //checks if box is empty
+    if (inName.value != "")
+    {
+        checkAddHighscore(inName.value, points)
+        pageHigh();
+    }
 });
+
+//HighScore Functions
+function checkAddHighscore(key, value){
+
+}
 
 
 //Other Functions
 function timeStart(){
+    //Setting Timer to 60 seconds
     mSecondsLeft = 6000;
 
-
+    //Timer set
     var timerInterval = setInterval(function() {
+        //updates time on website
         mSecondsLeft--;
         timer.textContent = (Math.round(mSecondsLeft * 10) / 1000).toFixed(2);
-    
+        
+        //checks if time runs out, clears timer
         if(mSecondsLeft <= 0) {
             clearInterval(timerInterval);
             pageEnd(points)
             timer.textContent = "00.00";
         }
 
+        //checks if page changed, clears timer
         if(page != 1){
             clearInterval(timerInterval);
             return;
@@ -151,12 +173,14 @@ function timePenalty(){
 }
 
 function giveQuestion(){
+    //generates question and pulls up "page"
     generateQuestionPage();
     pageQuiz();
 
-    //found this online, works like a charm
+    //Adds a event listener to each button
     document.querySelectorAll('#ansBtn').forEach(item => {
         item.addEventListener('click', clickevent => {
+            //checks whether the button text equals the answer and acts scordingly
             if (item.textContent == getObjectAnswer(currQuestion)){
                 points += 5;
             }
@@ -164,6 +188,7 @@ function giveQuestion(){
                 timePenalty();
             }
 
+            //Checks number of questions left and ends the test if none
             if (totalQ == 0){
                 points += Math.round(mSecondsLeft/100);
                 pageEnd(points);
